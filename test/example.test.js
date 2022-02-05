7a4833702226489035595fa8b815da74654b685d
Kamie Short <kamie.short@gmail.com>
2022-02-04 22:41:45 -0800

    corrected CI, test passing

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,34 @@
 // IMPORT MODULES under test here:
-import { renderMeal } from '../utils.js';
+import { renderMeal, renderSaveLi } from '../utils.js';
 
 const test = QUnit.test;
 
-test('renderMeal should return an <li> element', (expect) => {
+test('renderMeal should return an object with info', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = '<li>burrito - quantity 2 - ingredients are rice</li>';
+    const expected = '<li>Just a little - monday - ingredients are rice</li>';
     //Act
     // Call the function you're testing and set the result to a const
-    const actual = renderMeal('burrito' - 'quantity 1' - 'ingredients are rice');
+    const actual = renderMeal({
+        name: 'Just a little',
+        day: 'monday',
+        ingredient: 'rice',
+    });
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
 
+test('renderSaveLi should return an object with info', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = '<li>burrito - 1 item</li>';
+    //Act
+    // Call the function you're testing and set the result to a const
+    const actual = renderSaveLi({
+        input: 'burrito',
+        count: 1,
+    });
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual.outerHTML, expected);
